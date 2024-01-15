ac366d29dffd5ec4a3716fd6c5fa48898097357f
Unos <129419169+shoc71@users.noreply.github.com>
2024-01-14 22:40:53 -0500

    Update fast_unique_name_check.py

--- fast_unique_name_check.py
+++ fast_unique_name_check.py
@@ -1,12 +1,11 @@
-lirt = []
 filename = 'full_list_of_names.txt'
 
 with open (filename, mode='r', encoding='utf-8') as f:
-    line = f.readlines()
-    n_list = list(dict.fromkeys(line))
-# lirt = random.sample(lirt, len(lirt))
-lirt_str = ''.join(n_list).title()
+    lines = f.readlines()
+    lin_str = ''.join(list(dict.fromkeys(lines))).title()
+    # n_list = list(dict.fromkeys(line))
+    # lirt_str = ''.join(n_list).title()
 
 with open (filename, mode='w', encoding='utf-8') as f:
-    f.write(lirt_str)
+    f.write(lin_str)
 f.close()
