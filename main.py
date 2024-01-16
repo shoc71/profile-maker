6699cd0051bb69b0eb8086cc6dcfcf19026ae90f
Unos <129419169+shoc71@users.noreply.github.com>
2024-01-15 20:44:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from remove_tabs_n_spaces import *
+
+filename_test = 'test.txt'
+filename_adding_on = 'full_list_of_names.txt'
+empty_string = ''
+
+# def read_notepad(notepad_name):
+#     with open (notepad_name, 'r', encoding='utf-8') as f:
+#         lines = f.readlines()
+#     f.close()
+#     return lines
+
+def write_notepad(notepad_name, string):
+    with open(notepad_name, 'w', encoding='utf-8') as file:
+        file.write(string)
+    file.close()
+
+with open (filename_test, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+f.close()
+
+with open (filename_adding_on, 'r+', encoding='utf-8') as file:
+    more_lines = file.readlines()
+    mf_lines = more_lines + lines
+    merging_string = '\n'.join(mf_lines)
+f.close()
+
+# read_notepad(filename_test)
+# read_notepad(filename_adding_on)
+write_notepad(filename_test, empty_string)
+write_notepad(filename_adding_on, merging_string)
+
+from fast_unique_name_check import *
