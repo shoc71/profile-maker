551e23a881ebf5f826c817650e0fcc75100e6028
Unos <129419169+shoc71@users.noreply.github.com>
2024-05-19 01:28:24 -0400

    fixed fast_check, updated list

--- profile_resources/testing/fast_check.py
+++ profile_resources/testing/fast_check.py
@@ -1,20 +1,21 @@
-import os
+import random
 '''
 After crossing 100_000 unique names, the program began to slow down SIGNIFICANTLY.
 I wanted to quickly input the random names/words into the list and check if there were
 unique cases of it or not.
 '''
-# filepath = 'test.txt'
-filepath = r'C:\\Users\\Sonu\\Documents\\GitHub\\profile-maker\\profile_resources\\testing\\test.txt'
+filepath = 'profile_resources/names.txt'
 
 with open (filepath, 'r', encoding='utf-8') as f:
     lines = f.readlines()
-    
-print(len(lines))
-string_lines = ','.join(lines).title()
-line = string_lines.split(',')
-print(type(line))
-churning = set(string_lines)
-print(len(churning))
-    
-print(churning)
+    print(len(lines))
+    string_lines = ','.join(lines).title()
+    line = list(set(string_lines.split(',')))
+    shuffled_list = random.sample(line, len(line))
+f.close()
+
+with open (filepath, 'w', encoding='utf-8') as f:
+    print(len(shuffled_list))
+    writing_string_line = ''.join(shuffled_list).title()
+    f.write(writing_string_line)
+f.close()
