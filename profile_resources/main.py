8438646a887089879d2e1667d6da07cb3fa14a43
Unos <129419169+shoc71@users.noreply.github.com>
2024-07-28 21:50:43 -0400

    update on info and main is working

--- profile_resources/main.py
+++ profile_resources/main.py
@@ -21,36 +21,68 @@ All the names have been split among all the other notepads with each one contain
 filepath_for_input = 'profile_resources/input.txt'
 filepath_for_tracker = 'profile_resources/Important/tracker.txt'
 list_dir_of_names = 'profile_resources/names_list/'
-names_list = []
-names_of_name = []
 divisible_count = 100_000
+loop_1_total_name_count = 0
+loop_2_total_name_count = 0
 
 while True:
+    names_list = []
+    current_names = []
+
     files = os.listdir(list_dir_of_names)
 
     for file in files:
         names_list.append(list_dir_of_names + file)
 
     for names in names_list:
-        names_of_name += notepad.notepad_function(file=names, mode='r')
+        current_names += notepad.notepad_function(file=names, mode='r')
 
-    names_of_name += notepad.notepad_function(file=filepath_for_input, mode='r')
-    name_of_names_count = len(names_of_name)
+    string_lines = ','.join(current_names).title()
 
-    combined_names = list(set(names_of_name))
+    input_list = notepad.notepad_function(file=filepath_for_input, mode='r')
+    line = list(set(string_lines.split(',')))
+
+    combined_names = list(set(line + input_list))
     random.shuffle(combined_names)
+
+    current_names_count = len(current_names)
     combined_names_count = len(combined_names)
 
-    print(f"Before run: {name_of_names_count}\nAfter run: {combined_names_count}\n")
+    print(f"Before run: {current_names_count}\nAfter run: {combined_names_count}\n")
     notepad_count = int(round((combined_names_count / 100_000), 0) + 1) # rounding up
 
     print(f"Number of list names: {notepad_count}\n")
 
-    if (combined_names_count <= 0) and (name_of_names_count <= 0):
+    for i in range(1, notepad_count + 1):
+        if i <= 999:
+            file_name = f"{list_dir_of_names}names_{i:04d}.txt" # game breaking
+            combined_names = notepad.writing_into_file(total_list=combined_names, file_name=file_name, divisible_count=divisible_count)
+
+    combined_names = []
+
+    if (combined_names_count <= 0) and (current_names_count <= 0):
         print("Add some content in any of the files.")
         break
-    
-    for i in range(1, notepad_count + 1): # ranges end 1 before, so the +1 is to make up for it
+
+    elif (combined_names_count) != (current_names_count):
+        print(f"Difference of -[{combined_names_count - current_names_count}]- words bewteen Old and New Names.txt file(s).\n")
+
+    elif (combined_names_count) == (current_names_count): 
+        end_time = time.time()
+        total_run_time = program_tracker.formatted_runtime(start_time=start_time, end_time=end_time)
+        format_possibilities = '{:0.3e}'.format(current_names_count ** 2) # underrated way to round big numbers
+        print(f"Input.txt has been cleared and is ready to use.\n"
+                f"There are now -{format_possibilities}- number of possibilities, "
+                f"(Assuming a First and Last name only).\n"
+                f"Elapsed time: {total_run_time}.\n"
+                f"End of Program. No Changes to Report :)\n")
+                
+        # Keeping Track of all the instances this program has been run (for fun from now on.)
+        program_tracker.track_program_runs(filepath_for_tracker, combined_names_count, total_run_time)
+        break
+
+'''
+for i in range(1, notepad_count + 1): # ranges end 1 before, so the +1 is to make up for it
             if i <= 9:
                 file_name = (f"{list_dir_of_names}names_000{i}.txt")
                 combined_names = notepad.writing_into_file(total_list=combined_names, file_name=file_name, divisible_count=divisible_count)
@@ -62,20 +94,4 @@ while True:
             elif (i > 99) and (i <= 1000):
                 file_name = (f"{list_dir_of_names}names_0{i}.txt")
                 combined_names = notepad.writing_into_file(total_list=combined_names, file_name=file_name, divisible_count=divisible_count)
-
-    if (combined_names_count) != (name_of_names_count):
-        print(f"Difference of -[{combined_names_count - name_of_names_count}]- words bewteen Old and New Names.txt file(s).\n")
-
-    if (combined_names_count) == (name_of_names_count): 
-        end_time = time.time()
-        total_run_time = program_tracker.formatted_runtime(start_time=start_time, end_time=end_time)
-        format_possibilities = '{:0.3e}'.format(name_of_names_count ** 2) # underrated way to round big numbers
-        print(f"Input.txt has been cleared and is ready to use.\n"
-                f"There are now -{format_possibilities}- number of possibilities, "
-                f"(Assuming a First and Last name only).\n"
-                f"Elapsed time: {total_run_time}.\n"
-                f"End of Program. No Changes to Report :)\n")
-                
-        # Keeping Track of all the instances this program has been run (for fun from now on.)
-        program_tracker.track_program_runs(filepath_for_tracker, combined_names_count, total_run_time)
-        break
+'''
