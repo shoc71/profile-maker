72e5d26053d6558f377f6fbf9cfced50448efd90
shoc71 <supersonicwind6@gmail.com>
2024-12-04 16:40:29 -0500

    other names from server that wasn't backed

---
+++ profile_resources/Important/word_creation/voco_word_maker.py
@@ -0,0 +1,16 @@
+# vowel_then_consonant word maker
+
+# imports
+import string
+
+# constants
+VOWELS = 'aeiou'
+CONSONANTS = ''.join([char for char in string.ascii_lowercase if char not in VOWELS])
+
+# variables
+words = []
+
+# 
+def word_creation(length, words):
+    
+    pass
